4fabe788840b02b87a839d4ee3442e252552a606
sanket <sanketdeshmukh1080@gmail.com>
2021-12-10 20:02:57 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        int age;
+        String intrest;
+        String city;
+
+        System.out.print("Enter your name: ");
+        name= sc.nextLine();
+        System.out.print("Enter your intrests: ");
+        intrest= sc.nextLine();
+        System.out.print("Enter your city: ");
+        city= sc.nextLine();
+        System.out.print("Enter your age: ");
+        age= sc.nextInt();
+
+        System.out.println("Data added..");
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Intrest: "+intrest);
+        System.out.println("City: "+city);
+
+
+    }
+}
